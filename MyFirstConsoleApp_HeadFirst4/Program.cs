87c16b4b0a465e48f6b332a2a60150518b5c8f62
A-Munir <abdolla.munir@gmail.com>
2022-03-18 03:31:30 +0200

    updated readme file

---
+++ MyFirstConsoleApp_HeadFirst4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyFirstConsoleApp_HeadFirst4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello again from inside my first console app!");
+            Console.WriteLine("Ha ja");
+            Console.Beep();
+            // this is a comment
+
+            /*
+             this is a multi
+             line comment
+             */
+
+            // the next line gets rid off debug info following the output...comment it out to see the diffirence
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 C-SharpTutorialProjects
+### Exersises 
+https://csharpexercises.com/
+https://exercism.org/tracks/csharp
+https://projecteuler.net/ (not super relevent)
+https://www.codewars.com/join?language=csharp
+https://practity.com/resources-compilation/csharp/ (collection of good resources)
+### Road Maps
+https://github.com/MoienTajik/AspNetCore-Developer-Roadmap
+https://github.com/Elfocrash/.NET-Backend-Developer-Roadmap
+
+### Learning Resources 
+https://docs.microsoft.com/en-us/learn/modules/csharp-write-first/ (official from microsoft)
+
